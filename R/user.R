862ff04bae57d6e2f6713e19ccf5b13f840b1fd5
dmpe <cincenko@seznam.cz>
2016-03-06 15:59:51 +0100

    upload second method

--- R/user.R
+++ R/user.R
@@ -29,3 +29,45 @@ get_user_favorite <- function(username_for = "") {
 
   return(cbind(slideshow_id, tag_text))
 }
+
+
+#' @title Returns user contacts
+#'
+#' @examples
+#' asdqwdq <- get_user_contacts(username_for = "rashmi")
+#'
+#' @import httr
+#' @import xml2
+#'
+#' @export
+get_user_contacts <- function(username_for = "", limit = NULL, offset = NULL) {
+
+  queryarg = list(username_for = username_for,
+                  api_key = auth_api_key(NULL),
+                  hash = hash_concat(), ts = unix_timestamp())
+
+  url_response <- httr::GET(url = api_calls()[2], query = queryarg)
+  httr::stop_for_status(url_response)
+  raw_xml_content <- read_xml(content(url_response, as = "raw"))
+
+  username <- xml_find_all(raw_xml_content, "//Username")
+  numSlideshows <- xml_find_all(raw_xml_content, "//NumSlideshows")
+  numComments <- xml_find_all(raw_xml_content, "//NumComments")
+
+  username <- trimws(xml_text(username))
+  numSlideshows <- trimws(xml_text(numSlideshows))
+  numComments <- trimws(xml_text(numComments))
+
+  if (length(username) == 0 || length(numSlideshows) == 0) {
+    stop("User does not have any contacts")
+  }
+
+  return(cbind(username, numSlideshows, numComments))
+}
+
+
+
+
+
+
+
--- R/zzz.R
+++ R/zzz.R
@@ -5,12 +5,14 @@ api_calls <- function(){
   return(server_list)
 }
 
+#' @import openssl
 hash_concat <- function() {
   concatinatedString <- paste0(auth_shared_secret(NULL), unix_timestamp())
   hash <- openssl::sha1(concatinatedString)
   return(hash)
 }
 
+#' @import lubridate
 unix_timestamp <- function() {
-  return(round(as.numeric(now())))
+  return(round(as.numeric(lubridate::now())))
 }
